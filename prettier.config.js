0296943dfa17e280dae5aa278d0f5deb6cb4a88b
Antoine Cusset <acusset@palo-it.com>
2024-05-18 11:05:29 +0800

    style: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// Prettier configuration file
+const config = {
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
+
+export default config;
